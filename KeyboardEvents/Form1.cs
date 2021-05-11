8d4b3c27e3780d41ad50e0a3d36f99b55a945dfe
DELL <jaibymol4@gmail.com>
2021-05-11 20:58:45 +0530

    completed

---
+++ KeyboardEvents/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardEvents
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtHeading_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.KeyPreview = true;
+            //picBall.Location = panel1.Location;
+            // picBall.Location = this.Location;
+            
+            if (e.KeyCode == Keys.A)
+            {
+                picBall.Left = picBall.Left - 10;
+            }
+            else if (e.KeyCode == Keys.B)
+            {
+                picBall.Left = picBall.Left + 10;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                picBall.Top = picBall.Top - 10;
+            }
+            else if (e.KeyCode == Keys.F)
+            {
+                picBall.Top = picBall.Top + 10;
+            }
+            else if (e.KeyCode == Keys.C)
+            {
+                picBall.Location = new Point((panel1.Width / 2) - (picBall.Width / 2), (panel1.Height / 2) - (picBall.Height / 2));
+            }
+            
+}
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // picBall.Location = panel1.Location;
+           //if(picBall.Location <= panel1.Location)
+            
+           //     {
+           //     MessageBox.Show("cannot move");
+           //     } 
+
+            picBall.Focus();
+        }
+    }
+}
