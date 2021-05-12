4a61d24206af7067d492cc43443ffedd068c1236
DELL <jaibymol4@gmail.com>
2021-05-12 18:19:51 +0530

    updated

--- KeyboardEvents/Form1.Designer.cs
+++ KeyboardEvents/Form1.Designer.cs
@@ -31,73 +31,49 @@ namespace KeyboardEvents
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.picBall = new System.Windows.Forms.PictureBox();
-            this.txtHeading = new System.Windows.Forms.TextBox();
-            this.btnClose = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.picBall);
-            this.panel1.Location = new System.Drawing.Point(12, 66);
+            this.panel1.Location = new System.Drawing.Point(5, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 357);
+            this.panel1.Padding = new System.Windows.Forms.Padding(200, 185, 200, 185);
+            this.panel1.Size = new System.Drawing.Size(667, 611);
             this.panel1.TabIndex = 0;
             // 
             // picBall
             // 
             this.picBall.Image = global::KeyboardEvents.Properties.Resources.ball3;
-            this.picBall.Location = new System.Drawing.Point(301, 108);
+            this.picBall.Location = new System.Drawing.Point(275, 252);
+            this.picBall.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picBall.Name = "picBall";
-            this.picBall.Size = new System.Drawing.Size(106, 100);
+            this.picBall.Size = new System.Drawing.Size(107, 98);
             this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBall.TabIndex = 0;
             this.picBall.TabStop = false;
-            // 
-            // txtHeading
-            // 
-            this.txtHeading.BackColor = System.Drawing.Color.Fuchsia;
-            this.txtHeading.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtHeading.HideSelection = false;
-            this.txtHeading.Location = new System.Drawing.Point(12, 40);
-            this.txtHeading.Multiline = true;
-            this.txtHeading.Name = "txtHeading";
-            this.txtHeading.ReadOnly = true;
-            this.txtHeading.Size = new System.Drawing.Size(746, 26);
-            this.txtHeading.TabIndex = 1;
-            this.txtHeading.Text = "Running Ball";
-            this.txtHeading.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.txtHeading.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtHeading_KeyDown);
-            // 
-            // btnClose
-            // 
-            this.btnClose.BackColor = System.Drawing.Color.Fuchsia;
-            this.btnClose.Location = new System.Drawing.Point(755, 37);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(33, 29);
-            this.btnClose.TabIndex = 2;
-            this.btnClose.Text = "X";
-            this.btnClose.UseVisualStyleBackColor = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.txtHeading);
+            this.ClientSize = new System.Drawing.Size(677, 624);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KeyboardEvents";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -105,8 +81,6 @@ namespace KeyboardEvents
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox picBall;
-        private System.Windows.Forms.TextBox txtHeading;
-        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- KeyboardEvents/Form1.cs
+++ KeyboardEvents/Form1.cs
@@ -28,32 +28,49 @@ namespace KeyboardEvents
             this.KeyPreview = true;
             //picBall.Location = panel1.Location;
             // picBall.Location = this.Location;
+
             
-            if (e.KeyCode == Keys.A)
-            {
-                picBall.Left = picBall.Left - 10;
-            }
-            else if (e.KeyCode == Keys.B)
-            {
-                picBall.Left = picBall.Left + 10;
-            }
-            else if (e.KeyCode == Keys.W)
-            {
-                picBall.Top = picBall.Top - 10;
-            }
-            else if (e.KeyCode == Keys.F)
-            {
-                picBall.Top = picBall.Top + 10;
-            }
-            else if (e.KeyCode == Keys.C)
-            {
-                picBall.Location = new Point((panel1.Width / 2) - (picBall.Width / 2), (panel1.Height / 2) - (picBall.Height / 2));
-            }
-            
+            //if (x <= panel1.Width-50)
+            //{
+               
+           // }
 }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-           
+            int x = picBall.Location.X;
+            int y = picBall.Location.Y;
+            if (x >= 25)
+            {
+                if (e.KeyCode == Keys.A)
+                {
+                    picBall.Left = picBall.Left - 10;
+                }
+            }
+            if (x <= 400)
+            {
+                if (e.KeyCode == Keys.B)
+                {
+                    picBall.Left = picBall.Left + 10;
+                }
+            }
+            if (y>= 25)
+            {
+                if (e.KeyCode == Keys.W)
+                {
+                    picBall.Top = picBall.Top - 10;
+                }
+            }
+            if (y<= 400)
+            {
+                if (e.KeyCode == Keys.F)
+                {
+                    picBall.Top = picBall.Top + 10;
+                }
+            }
+             if (e.KeyCode == Keys.C)
+            {
+                picBall.Location = new Point((panel1.Width / 2) - (picBall.Width / 2), (panel1.Height / 2) - (picBall.Height / 2));
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
